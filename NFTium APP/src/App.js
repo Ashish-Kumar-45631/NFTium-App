8b75fbf76811ac833c4586ca85a7bc94b5a6397d
Ashish Kumar <143773138+Ashish-Kumar-45631@users.noreply.github.com>
2024-08-03 09:57:34 +0530

    Add files via upload

---
+++ NFTium APP/src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { Component } from "react";
+import { Hero } from "./Hero";
+import { About } from "./About";
+import { Navbar } from "./Navbar";
+
+
+export default function App() {
+  // Write this code in Navbar.js file
+  
+
+  // Write this code in Hero.js file
+
+
+  // Write this code in About.js file
+  
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
